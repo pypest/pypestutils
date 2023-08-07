7aa3e1e175673d2011b6bf26439f71f21463266c
Mike Taves <mwtoews@gmail.com>
2023-08-07 13:20:25 +1200

    Change scripts to install to pypestutils/lib; add finder.load() to guess

--- scripts/build_lib.bat
+++ scripts/build_lib.bat
@@ -4,8 +4,13 @@ cd %~dp0\..
 :: clean previous attempts
 RD /S /Q builddir
 RD /S /Q inst
+RD /S /Q pypestutils\lib
 
 :: setup, compile and install
-meson setup builddir --prefix=%cd%\inst --libdir=lib
+meson setup builddir --prefix=%cd%\inst
 meson compile -C builddir
 meson install -C builddir
+
+::copy lib files to Python module
+MD pypestutils\lib
+COPY /B inst\bin\libppu.dll pypestutils\lib
--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -7,8 +7,17 @@ cd $(dirname $0)/..
 # clean previous attempts
 rm -rf builddir
 rm -rf inst
+rm -rf pypestutils/lib
 
 # setup, compile and install
 meson setup builddir --prefix=$(pwd)/inst --libdir=lib
 meson compile -C builddir
 meson install -C builddir
+
+# copy lib files to Python module
+mkdir pypestutils/lib
+if [ "$(uname)" == "Darwin" ]; then
+  cp inst/lib/libppu.dylib pypestutils/lib/
+else
+  cp inst/lib/libppu.so pypestutils/lib/
+fi
