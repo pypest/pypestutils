1a2a13774057ce12f8313dc5fd240c75ca796803
Mike Taves <mwtoews@gmail.com>
2023-08-09 13:43:45 +1200

    Several updates to PestUtilsLib:
    * Add free_all_memory and interp_from_structured_grid
    * Use lowercase parameters
    * Share some array checking with _check_interp_arrays()

--- pypestutils/core.py
+++ pypestutils/core.py
@@ -16,7 +16,7 @@ from .logger import get_logger
 _dimvar_cache = {}
 
 
-class PestUtilsLibException(BaseException):
+class PestUtilsLibError(BaseException):
     """Exception from PestUtilsLib."""
 
     pass
@@ -38,19 +38,27 @@ class PestUtilsLib:
         self.logger.debug("added prototypes")
 
     def __del__(self):
+        """Clean-up library instance."""
         try:
-            self.logger.debug("calling free_all_memory()")
-            self.lib.free_all_memory()
-        except AttributeError as err:
+            self.free_all_memory()
+        except (AttributeError, PestUtilsLibError) as err:
             if hasattr(self, "logger"):
-                self.logger.warning("%s", err)
+                self.logger.warning("cannot call __del__: %s", err)
 
     def get_char_array(self, name: str):
         """Get c_char Array with a fixed size from dimvar."""
         return get_char_array(self.lib, name)
 
     def create_char_array(self, init: str | bytes, name: str):
-        """Create c_char Array with a fixed size from dimvar and intial value."""
+        """Create c_char Array with a fixed size from dimvar and intial value.
+
+        Parameters
+        ----------
+        init : str or bytes
+            Initial value.
+        name : str
+            Variable length name, e.g. LENFILENAME or LENVARTYPE.
+        """
         if isinstance(init, str):
             init = init.encode()
         elif isinstance(init, bytes):
@@ -65,14 +73,14 @@ class PestUtilsLib:
         return get_dimvar_int(self.lib, name)
 
     def inquire_modflow_binary_file_specs(
-        self, FileIn: str | PathLike, FileOut: str | PathLike, isim: int, itype: int
+        self, filein: str | PathLike, fileout: str | PathLike, isim: int, itype: int
     ) -> dict:
         """Report some of the details of a MODFLOW-written binary file.
 
         Parameters
         ----------
-        FileIn : str or PathLike
-        FileOut : str or PathLike
+        filein : str or PathLike
+        fileout : str or PathLike
         isim : int
             Simulator.
         itype : int
@@ -80,19 +88,23 @@ class PestUtilsLib:
 
         Returns
         -------
-        dict
-            With keys: ["iprec", "narray", "ntime"]
+        iprec : int
+            Where 1 = single; 2 = double.
+        narray : int
+            Number of arrays.
+        ntime : int
+            Number of times.
         """
-        FileIn = Path(FileIn)
-        if not FileIn.is_file():
-            raise FileNotFoundError(f"could not find FileIn {FileIn}")
-        FileOut = Path(FileOut)  # TODO
+        filein = Path(filein)
+        if not filein.is_file():
+            raise FileNotFoundError(f"could not find filein {filein}")
+        fileout = Path(fileout)  # TODO
         iprec = c_int()
         narray = c_int()
         ntime = c_int()
         res = self.lib.inquire_modflow_binary_file_specs(
-            byref(self.create_char_array(bytes(FileIn), "LENFILENAME")),
-            byref(self.create_char_array(bytes(FileOut), "LENFILENAME")),
+            byref(self.create_char_array(bytes(filein), "LENFILENAME")),
+            byref(self.create_char_array(bytes(fileout), "LENFILENAME")),
             byref(c_int(isim)),
             byref(c_int(itype)),
             byref(iprec),
@@ -100,8 +112,8 @@ class PestUtilsLib:
             byref(ntime),
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
-        self.logger.info("inquired modflow binary file specs from %r", FileIn.name)
+            raise PestUtilsLibError(self.retrieve_error_message())
+        self.logger.info("inquired modflow binary file specs from %r", filein.name)
         return {
             "iprec": iprec.value,
             "narray": narray.value,
@@ -156,7 +168,7 @@ class PestUtilsLib:
             delc.astype(np.float64),
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
+            raise PestUtilsLibError(self.retrieve_error_message())
         self.logger.info("installed strictured grid %r from specs", gridname)
 
     def uninstall_structured_grid(self, gridname: str) -> None:
@@ -171,8 +183,127 @@ class PestUtilsLib:
             byref(self.create_char_array(gridname, "LENGRIDNAME"))
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
+            raise PestUtilsLibError(self.retrieve_error_message())
         self.logger.info("uninstalled strictured grid %r", gridname)
+
+    def free_all_memory(self) -> None:
+        """Deallocate all memory that is being used."""
+        ret = self.lib.free_all_memory()
+        if ret != 0:
+            raise PestUtilsLibError(self.retrieve_error_message())
+        self.logger.info("all memory was freed up")
+
+    def _check_interp_arrays(
+        self,
+        ecoord: npt.NDArray[np.float64],
+        ncoord: npt.NDArray[np.float64],
+        layer: npt.NDArray[np.int32],
+    ) -> int:
+        """Check point interpolation arrays before passing to Fortran.
+
+        Returns
+        -------
+        int
+            npts
+        """
+        if layer.ndim != 1:
+            raise ValueError("expected 'layer' to have ndim=1")
+        npts = len(layer)
+        expected_shape = layer.shape
+        if npts <= 0:
+            raise ValueError("expected 'layer' with length greater than zero")
+        elif not np.issubdtype(layer.dtype, np.integer):
+            raise ValueError(
+                f"expected 'layer' to be integer type; found {layer.dtype}"
+            )
+        elif ecoord.shape != expected_shape:
+            raise ValueError(f"expected 'ecoord' shape to be {expected_shape}")
+        elif ncoord.shape != expected_shape:
+            raise ValueError(f"expected 'ncoord' shape to be {expected_shape}")
+        return npts
+
+    def interp_from_structured_grid(
+        self,
+        gridname: str,
+        depvarfile: str | PathLike,
+        isim: int,
+        iprec: int,
+        ntime: int,
+        vartype: str,
+        interpthresh: float,
+        nointerpval: float,
+        ecoord: npt.NDArray[np.float64],
+        ncoord: npt.NDArray[np.float64],
+        layer: npt.NDArray[np.int32],
+    ) -> dict:
+        """Spatial interpolate points from a structured grid.
+
+        Parameters
+        ----------
+        gridname : str
+            Name of installed structured grid.
+        depvarfile : str or PathLike
+            Name of binary file to read.
+        isim : int
+            Specify -1 for MT3D; 1 for MODFLOW.
+        iprec : int
+            Specify -1 for MT3D; 1 for MODFLOW.
+        ntime : int
+            Number of output times.
+        vartype : str
+            Only read arrays of this type.
+        interpthresh : float
+            Absolute threshold for dry or inactive.
+        nointerpval : float
+            Value to use where interpolation is not possible.
+        ecoord, ncoord : array_like
+            X/Y or Easting/Northing coordinates for points.
+        layer : array_like
+            Layers of points.
+
+        Returns
+        -------
+        nproctime : int
+            Number of processed simulation times.
+        simtime : np.ndarray
+            Simulation time.
+        simstate : np.ndarray
+            Interpolated system state with dimensions (ntime,npts).
+        """
+        depvarfile = Path(depvarfile)
+        if not depvarfile.is_file():
+            raise FileNotFoundError(f"could not find depvarfile {depvarfile}")
+        npts = self._check_interp_arrays(ecoord, ncoord, layer)
+        simtime = np.zeros(ntime, np.float64)
+        simstate = np.zeros((ntime, npts), np.float64, "F")
+        c_nproctime = c_int()
+        res = self.lib.interp_from_structured_grid(
+            byref(self.create_char_array(gridname, "LENGRIDNAME")),
+            byref(self.create_char_array(bytes(depvarfile), "LENFILENAME")),
+            byref(c_int(isim)),
+            byref(c_int(iprec)),
+            byref(c_int(ntime)),
+            byref(self.create_char_array(vartype, "LENVARTYPE")),
+            byref(c_double(interpthresh)),
+            byref(c_double(nointerpval)),
+            byref(c_int(npts)),
+            ecoord.astype(np.float64),
+            ncoord.astype(np.float64),
+            layer.astype(np.int32),
+            byref(c_nproctime),
+            simtime,
+            simstate,
+        )
+        if res != 0:
+            raise PestUtilsLibError(self.retrieve_error_message())
+        self.logger.info(
+            "interpolated %d points from structured grid %r", npts, gridname
+        )
+        return {
+            "nproctime": c_nproctime.value,
+            "simtime": simtime,
+            "simstate": simstate,
+        }
 
     def install_mf6_grid_from_file(
         self, gridname: str, grbfile: str | PathLike
@@ -211,7 +342,7 @@ class PestUtilsLib:
             byref(ndim3),
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
+            raise PestUtilsLibError(self.retrieve_error_message())
         self.logger.info(
             "installed mf6 grid %r from grbfile=%r", gridname, grbfile.name
         )
@@ -235,7 +366,7 @@ class PestUtilsLib:
             byref(self.create_char_array(gridname, "LENGRIDNAME"))
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
+            raise PestUtilsLibError(self.retrieve_error_message())
         self.logger.info("uninstalled mf6 grid %r", gridname)
 
     def calc_mf6_interp_factors(
@@ -249,24 +380,12 @@ class PestUtilsLib:
         blnfile: str | PathLike,
     ) -> npt.NDArray[np.int32]:
         """Calculate interpolation factors from a MODFLOW 6 DIS or DISV."""
-        c_gridname = self.create_char_array(gridname, "LENGRIDNAME")
         factorfile = Path(factorfile)  # TODO
         blnfile = Path(blnfile)  # TODO
-        if layer.ndim != 1:
-            raise ValueError("expected 'layer' to have ndim=1")
-        npts = len(layer)
-        expected_shape = layer.shape
-        if npts <= 0:
-            raise ValueError("expected 'layer' with length greater than zero")
-        elif not np.issubdtype(layer.dtype, np.integer):
-            raise ValueError("expected 'layer' to be integer type")
-        elif ecoord.shape != expected_shape:
-            raise ValueError(f"expected 'ecoord' shape to be {expected_shape}")
-        elif ncoord.shape != expected_shape:
-            raise ValueError(f"expected 'ncoord' shape to be {expected_shape}")
+        npts = self._check_interp_arrays(ecoord, ncoord, layer)
         interp_success = np.zeros(npt, np.int32)
         res = self.lib.calc_mf6_interp_factors(
-            byref(c_gridname),
+            byref(self.create_char_array(gridname, "LENGRIDNAME")),
             byref(c_int(npts)),
             ecoord.astype(np.float64),
             ncoord.astype(np.float64),
@@ -277,6 +396,6 @@ class PestUtilsLib:
             interp_success,
         )
         if res != 0:
-            raise PestUtilsLibException(self.retrieve_error_message())
+            raise PestUtilsLibError(self.retrieve_error_message())
         self.logger.info("calculated mf6 interp factors for %r", gridname)
         return interp_success
--- pypestutils/ctypes_declarations.py
+++ pypestutils/ctypes_declarations.py
@@ -1,4 +1,5 @@
 """Low-level Fortran-Python ctypes functions."""
+from __future__ import annotations
 
 from ctypes import ARRAY, CDLL, POINTER, c_char, c_double, c_int
 
@@ -7,14 +8,18 @@ from numpy.ctypeslib import ndpointer
 # Cache variables by uppercase dimvar name
 _dimvar_cache = {}
 _char_array_cache = {}
+_misc_lengths = {
+    "LENVARTYPE": 17,
+}
 
 
 def get_dimvar_int(lib: CDLL, name: str) -> int:
     """Get dimvar constant integer from library instance."""
-    try:
+    if name in _dimvar_cache:
         return _dimvar_cache[name]
-    except KeyError:
-        pass
+    elif name in _misc_lengths:
+        # Special consideration for constants not specified by dimvar
+        return _misc_lengths[name]
     c_var = c_int.in_dll(lib, name)
     _dimvar_cache[name] = c_var.value
     return c_var.value
@@ -22,11 +27,10 @@ def get_dimvar_int(lib: CDLL, name: str) -> int:
 
 def get_char_array(lib: CDLL, name: str):
     """Get c_char array with a fixed size from dimvar."""
-    try:
+    if name in _char_array_cache:
         return _char_array_cache[name]
-    except KeyError:
-        pass
-    array_type = ARRAY(c_char, get_dimvar_int(lib, name))
+    size = get_dimvar_int(lib, name)
+    array_type = ARRAY(c_char, size)
     _char_array_cache[name] = array_type
     return array_type
 
@@ -37,10 +41,10 @@ def prototype(lib):
     lenfilename_t = get_char_array(lib, "LENFILENAME")
     lenmessage_t = get_char_array(lib, "LENMESSAGE")
     lengridname_t = get_char_array(lib, "LENGRIDNAME")
+    lenvartype_t = get_char_array(lib, "LENVARTYPE")
 
     # inquire_modflow_binary_file_specs(
     #   FileIn,FileOut,isim,itype,iprec,narray,ntime)
-    lib.inquire_modflow_binary_file_specs.restype = c_int
     lib.inquire_modflow_binary_file_specs.argtypes = (
         POINTER(lenfilename_t),  # FileIn, in
         POINTER(lenfilename_t),  # FileOut, in
@@ -50,16 +54,14 @@ def prototype(lib):
         POINTER(c_int),  # narray, out
         POINTER(c_int),  # ntime, out
     )
+    lib.inquire_modflow_binary_file_specs.restype = c_int
 
     # retrieve_error_message(errormessage)
+    lib.retrieve_error_message.argtypes = (POINTER(lenmessage_t),)  # errormessage, out
     lib.retrieve_error_message.restype = c_int
-    lib.retrieve_error_message.argtypes = (
-        POINTER(lenmessage_t),  # errormessage, out
-    )
 
     # install_structured_grid(
     #   gridname,ncol,nrow,nlay,icorner,e0,n0,rotation,delr,delc)
-    lib.install_structured_grid.restype = c_int
     lib.install_structured_grid.argtypes = (
         POINTER(lengridname_t),  # gridname, in
         POINTER(c_int),  # ncol, in
@@ -72,20 +74,40 @@ def prototype(lib):
         ndpointer(c_double, ndim=1),  # delr(ncol), in
         ndpointer(c_double, ndim=1),  # delc(nrow), in
     )
+    lib.install_structured_grid.restype = c_int
 
     # uninstall_structured_grid(gridname)
+    lib.uninstall_structured_grid.argtypes = (POINTER(lengridname_t),)  # gridname, in
     lib.uninstall_structured_grid.restype = c_int
-    lib.uninstall_structured_grid.argtypes = (
-        POINTER(lengridname_t),  # gridname, in
-    )
 
     # free_all_memory()
-    lib.free_all_memory.restype = c_int
     lib.free_all_memory.argtypes = ()
+    lib.free_all_memory.restype = c_int
+
+    # interp_from_structured_grid(
+    #   GridName,DepVarFile,isim,iprec,ntime,VarType,InterpThresh,NoInterpVal,
+    #   npts,ecoord,ncoord,layer,nproctime,simtime,simstate)
+    lib.interp_from_structured_grid.argtypes = (
+        POINTER(lengridname_t),  # GridName, in
+        POINTER(lenfilename_t),  # DepVarFile, in
+        POINTER(c_int),  # isim, in
+        POINTER(c_int),  # iprec, in
+        POINTER(c_int),  # ntime, in
+        POINTER(lenvartype_t),  # VarType, in
+        POINTER(c_double),  # InterpThresh, in
+        POINTER(c_double),  # NoInterpVal, in
+        POINTER(c_int),  # npts, in
+        ndpointer(c_double, ndim=1),  # ecoord, in
+        ndpointer(c_double, ndim=1),  # ncoord, in
+        ndpointer(c_int, ndim=1),  # layer, in
+        POINTER(c_int),  # nproctime, out
+        ndpointer(c_double, ndim=1),  # simtime(ntime), out
+        ndpointer(c_double, ndim=2, flags="F"),  # simstate(ntime,npts), out
+    )
+    lib.interp_from_structured_grid.restype = c_int
 
     # install_mf6_grid_from_file(
     #   gridname,grbfile,idis,ncells,ndim1,ndim2,ndim3)
-    lib.install_mf6_grid_from_file.restype = c_int
     lib.install_mf6_grid_from_file.argtypes = (
         POINTER(lengridname_t),  # gridname, in
         POINTER(lenfilename_t),  # grbfile, in
@@ -95,17 +117,15 @@ def prototype(lib):
         POINTER(c_int),  # ndim2, out
         POINTER(c_int),  # ndim3, out
     )
+    lib.install_mf6_grid_from_file.restype = c_int
 
     # uninstall_mf6_grid(gridname)
+    lib.uninstall_mf6_grid.argtypes = (POINTER(lengridname_t),)  # gridname, in
     lib.uninstall_mf6_grid.restype = c_int
-    lib.uninstall_mf6_grid.argtypes = (
-        POINTER(lengridname_t),  # gridname, in
-    )
 
     # calc_mf6_interp_factors(
     #   gridname,npts,ecoord,ncoord,layer,factorfile,
     #   factorfiletype,blnfile,interp_success)
-    lib.calc_mf6_interp_factors.restype = c_int
     lib.calc_mf6_interp_factors.argtypes = (
         POINTER(lengridname_t),  # gridname, in
         POINTER(c_int),  # npts, in
@@ -117,3 +137,4 @@ def prototype(lib):
         POINTER(lenfilename_t),  # blnfile, in
         ndpointer(c_int, ndim=1),  # interp_success, out
     )
+    lib.calc_mf6_interp_factors.restype = c_int
