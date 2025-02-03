141804e5d6619170f8d489f49e5a8841f101ef06
Mike Taves <mwtoews@gmail.com>
2025-02-03 22:03:08 +1300

    Expand logic for adding -ld_classic flag, if present and not deprecated

--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -4,15 +4,26 @@ set -e
 # always run from top of repo
 cd $(dirname $0)/..
 
-
-
 # this needs bash
 case "$OSTYPE" in
-  darwin*)  libname=lib/libpestutils.dylib && export LDFLAGS="$LDFLAGS -Wl,-ld_classic";;
+  darwin*)  libname=lib/libpestutils.dylib ;;
   linux*)   libname=lib/libpestutils.so ;;
   msys* )   libname=bin/pestutils.dll ;;
   *) echo "unknown \$OSTYPE: $OSTYPE" && exit 1 ;;
 esac
+
+# on macOS add "-ld_classic" to LDFLAGS if ld-classic is present and not deprecated
+if [[ $OSTYPE == darwin* ]]; then
+  XCODE_PATH=$(/usr/bin/xcode-select -p 2> /dev/null)
+  if [ -n $XCODE_PATH ]; then
+    if [ -x "$XCODE_PATH/usr/bin/ld-classic" -o \
+         -x "$XCODE_PATH/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld-classic" ]; then
+      if [ -z "$(ld -ld_classic 2>&1 | grep 'ld_classic is deprecated')" ]; then
+        export LDFLAGS="$LDFLAGS -Wl,-ld_classic"
+      fi
+    fi
+  fi
+fi
 
 # clean previous attempts
 rm -rf builddir
